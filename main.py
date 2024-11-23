6626e5e78e80df0f8f0def072c49dc020dfb90f9
RisenThanatos <brandon.schick@hotmail.com>
2024-11-23 16:15:42 +0100

    added 2 more functions(convert_to_list, create_report) to main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+def create_report(word_count, char_list, filepath):
+    report = f"--- Begin report of {filepath} ---\n"
+    report += f"{word_count} words found in the document\n\n"
+
+    for char_dict in char_list:
+        report += f"The {char_dict['char']} character was found {char_dict['count']} times\n"
+
+    report += "--- End report ---"
+    return report
+
+
+def convert_to_list(char_dict):
+    char_list = []
+    for char, count in char_dict.items():
+        if char.isalpha():
+            char_list.append({"char": char, "count": count})
+    char_list.sort(reverse=True, key=lambda x: x["count"])
+    return char_list
+
+
 def count_characters(text):
     characters = {}
     lowercase = text.lower()
@@ -6,25 +26,32 @@ def count_characters(text):
             characters[char] += 1
         else:
             characters[char] = 1
-    print(characters)
+    return characters
+
 
 def count_words(text):
     words = text.strip().split()
     count = 0
     for word in words:
         count += 1
-    print(count)
+    return count
+
 
 def main():
 
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+        filepath = f.name
 
-        count_characters(file_contents)
+        word_count = count_words(file_contents)
 
-        count_words(file_contents)
+        char_dict = count_characters(file_contents)
 
-    print(file_contents) # replace "file_contents" with "1" here for testing
+        char_list = convert_to_list(char_dict)
+
+        report = create_report(word_count, char_list, filepath)
+
+        print(report)
 
 
 main()
